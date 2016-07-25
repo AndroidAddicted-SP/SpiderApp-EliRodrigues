ea7b2242c8c7dbd1edded5ef045a2e52f8aa152f
dsreliete <dsreliete@gmail.com>
2016-07-25 16:51:38 -0300

    layout changes

--- app/src/main/java/com/challenge/spiderapp/ComicsDetail/DetailActivity.java
+++ app/src/main/java/com/challenge/spiderapp/ComicsDetail/DetailActivity.java
@@ -2,9 +2,12 @@ package com.challenge.spiderapp.ComicsDetail;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ProgressBar;
 
 import com.challenge.spiderapp.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -12,11 +15,27 @@ import butterknife.ButterKnife;
  */
 public class DetailActivity extends AppCompatActivity {
 
+    public static final String TAG = DetailActivity.class.getSimpleName();
+
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_comics);
 
         ButterKnife.bind(this);
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
 }
